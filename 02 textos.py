717440592500a28291229812eb41e34c8aa3bd10
Luis Guilherme Alves Rodrigues <luis.rodrigues@bsb.ferreiraechagas.local>
2026-09-03 14:41:41 -0300

    primeiro commit

--- 02 textos.py
+++ 02 textos.py
@@ -3,6 +3,6 @@ custo = 600
 
 lucro = faturamento - custo
 #texto = " O lucro foi de " + str(lucro) + " e o faturamentofoi de " + str(faturamento)
-texto = f"O lucro foi de {lucro} e o faturamento foi de {faturamento}"
+texto = f"O lucro foi de R${lucro} e o faturamento foi de R${faturamento}"
 
 print (texto)
